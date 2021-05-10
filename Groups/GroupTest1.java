1386a0a6a01cb06ff4b3426d34092dcad148c9f7
Pasindu Ukwatta <52557884+PasinduUkwatta@users.noreply.github.com>
2021-05-10 10:19:44 +0530

    TestNG -Group

---
+++ Groups/GroupTest1.java
@@ -0,0 +1,25 @@
+package Groups;
+
+import org.testng.annotations.Test;
+
+public class GroupTest1 {
+    @Test(groups = {"smoke","reg"})
+    public void testGT1_1() {
+        System.out.println("GT1_1");
+    }
+
+    @Test(groups = "reg")
+    public void testGT1_2() {
+        System.out.println("GT1_2");
+    }
+
+    @Test
+    public void testGT1_3() {
+        System.out.println("GT1_3");
+    }
+
+    @Test(groups = {"smoke"})
+    public void testGT1_4() {
+        System.out.println("GT1_4");
+    }
+}
---
+++ Groups/GroupTest3.java
@@ -0,0 +1,30 @@
+package Groups;
+
+import org.testng.annotations.Test;
+
+public class GroupTest3 {
+    @Test(groups = {"smoke", "reg"})
+    public void testGT3_1() {
+        System.out.println("GT3_1");
+    }
+
+    @Test
+    public void testGT3_2() {
+        System.out.println("GT3_2");
+    }
+
+    @Test(groups = {"reg"})
+    public void testGT3_3() {
+        System.out.println("GT3_3");
+    }
+
+    @Test(groups = {"reg"})
+    public void testGT13_4() {
+        System.out.println("GT3_4");
+    }
+
+    @Test(groups = {"smoke"})
+    public void testGT3_5() {
+        System.out.println("GT3_5");
+    }
+}
